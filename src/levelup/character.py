18108968f25818a24fef640165ea9280aef1eb05
Alex Buhse <abuhse32@gmail.com>
2023-06-15 18:55:44 +0000

    implemented get position

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,9 @@
 class Character:
     name = ""
 
+
     def __init__(self, character_name):
         self.name = character_name
+
+    def get_name(self):
+        return self.name
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,8 +1,23 @@
 from unittest import TestCase
 from levelup.character import Character
 
+ARBITRARY_NAME = "MyName"
+
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        self.assertEqual(ARBITRARY_NAME, testobj.name)
+        test_obj = Character(ARBITRARY_NAME)
+        self.assertEqual(ARBITRARY_NAME, test_obj.name)
+
+    def test_get_name(self):
+        test_obj = Character(ARBITRARY_NAME)
+        self.assertEqual(ARBITRARY_NAME, test_obj.get_name())
+
+    # def test_get_position(self):
+    #     test_obj = Character(ARBITRARY_NAME)
+    #     self.assertEqual((0,0), test_obj.get_position())
+
+    # def test_move(self):
+    #     test_obj = Character(ARBITRARY_NAME)
+    #     direction = 'n'
+    #     test_obj.move(direction)
+    #     self.assertEqual((0,1), test_obj.get_position())
