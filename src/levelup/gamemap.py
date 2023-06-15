d89ddeb06d94c5fa25bd18044e441eda21ec9187
Kat Stubblefield <khstubblefield@gmail.com>
2023-06-15 19:21:32 +0000

    Added IsPositionValid() to GameMap

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
+from levelup.position import Position
 
 @dataclass
 class GameMap:
     x = ""
+
+    def IsPositionValid(self, position: Position):
+        return
--- tests/test_map_controller.py
+++ tests/test_map_controller.py
@@ -1,8 +1,13 @@
 from unittest import TestCase
 from levelup.gamemap import GameMap
+from levelup.position import Position
 
 class TestGameMap(TestCase):
     def test_init(self):
         testObj = GameMap()
         assert testObj != None
         
+    def test_isPositionValid(self):
+        testObj = GameMap()
+        testPosition = Position(0, 0)
+        assert testObj.IsPositionValid(testPosition) != ""
