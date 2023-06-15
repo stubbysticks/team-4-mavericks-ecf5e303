516020e354dbbdda6714de42ecdf139e38ffb3dd
Larry Povolotsky <lpovolot@gmail.com>
2023-06-15 19:31:19 +0000

    get character position

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,9 +1,15 @@
+from levelup.position import Position
+
+
 class Character:
     name = ""
-
+    position = Position(0, 0)
 
     def __init__(self, character_name):
         self.name = character_name
 
     def get_name(self):
         return self.name
+
+    def get_position(self):
+        return self.position
--- tests/test_character.py
+++ tests/test_character.py
@@ -12,9 +12,12 @@ class TestCharacterInitWithName(TestCase):
         test_obj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, test_obj.get_name())
 
-    # def test_get_position(self):
-    #     test_obj = Character(ARBITRARY_NAME)
-    #     self.assertEqual((0,0), test_obj.get_position())
+    def test_get_character_position(self):
+        test_obj = Character(ARBITRARY_NAME)
+        ARBITRARY_X = 0
+        ARBITRARY_Y = 0
+        self.assertEqual(ARBITRARY_X, test_obj.get_position().get_position()[0])
+        self.assertEqual(ARBITRARY_X, test_obj.get_position().get_position()[1])
 
     # def test_move(self):
     #     test_obj = Character(ARBITRARY_NAME)
